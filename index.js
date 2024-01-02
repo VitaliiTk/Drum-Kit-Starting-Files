378b682be78f37daa60e26f4450feea4d41d52ea
Vitalii <tka4uk.gt41@gmail.com>
2024-01-03 01:28:35 +0600

    update

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
+function audioPlay(name) {
+    switch (name) {
+        case 'w':
+            const w = new Audio('./sounds/tom-1.mp3')
+            w.play()
+            break
+        case 'a':
+            const a = new Audio('./sounds/tom-2.mp3')
+            a.play()
+            break
+        case 's':
+            const s = new Audio('./sounds/tom-3.mp3')
+            s.play()
+            break
+        case 'd':
+            const d = new Audio('./sounds/tom-4.mp3')
+            d.play()
+            break
+        case 'j':
+            const j = new Audio('./sounds/snare.mp3')
+            j.play()
+            break
+        case 'k':
+            const k = new Audio('./sounds/crash.mp3')
+            k.play()
+            break
+        case 'l':
+            const l = new Audio('./sounds/kick-bass.mp3')
+            l.play()
+            break
+        default: console.log(name);
+    }
+}
+
+// Detecting button press
 const btns = document.querySelectorAll('.drum')
-const w = new Audio('./sounds/tom-1.mp3'),
-    a = new Audio('./sounds/tom-2.mp3'),
-    s = new Audio('./sounds/tom-3.mp3'),
-    d = new Audio('./sounds/tom-4.mp3'),
-    j = new Audio('./sounds/snare.mp3'),
-    k = new Audio('./sounds/crash.mp3'),
-    l = new Audio('./sounds/kick-bass.mp3')
-
-// btns.forEach((element, index) => {
-//     element.addEventListener('click', event => {
-//         alert('Click ' + index)
-//     })
-// })
-
 for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener('mousedown', function () {
-        this.style.color = 'white'
+    btns[i].addEventListener('mousedown', function (e) {
         const buttonText = this.textContent
-        console.log(buttonText);
-        switch (buttonText) {
-            case 'w':
-                w.play()
-                break
-            case 'a':
-                a.play()
-                break
-            case 's':
-                s.play()
-                break
-            case 'd':
-                d.play()
-                break
-            case 'j':
-                j.play()
-                break
-            case 'k':
-                k.play()
-                break
-            case 'l':
-                l.play()
-                break
-            default: console.log(buttonText);
-        }
+        audioPlay(buttonText)
     })
 }
+
+// Detecting Keyboard press
+document.addEventListener('keydown', function (event) {
+    const key = event.key
+    audioPlay(key)
+})
